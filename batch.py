862cf40d66bfcac039f710af60cf2f2a9ea0374d
dflay <flay@umass.edu>
2015-12-22 16:21:22 -0600

    Added python scripts that allow for batch running.

---
+++ batch.py
@@ -0,0 +1,103 @@
+#! /usr/bin/python 
+
+
+# TODO: 1. Add a way to choose an existing configuration
+#       2. If a desired configuration doesn't exist, provide
+#          instructions on how to make one in the GUI.
+
+# test out using screen in a python script to do a few things
+
+import os
+import time
+import sys
+from confmgr import ConfigManager
+from timeit import default_timer
+
+home = os.getcwd()
+config_name = ""
+myConfig = ConfigManager(home,config_name) 
+
+# commands 
+run_cmd      = "./run_nmr.sh"
+list_configs = "ls ./input/configs/" 
+config_dir   = home + "/input/configs"
+
+filenames = next( os.walk(config_dir) )[2]
+
+# some units 
+sec    = 1
+ms     = 1E-3*sec
+us     = 1E-6*sec
+minute = 60*sec; 
+hr     = 60*minute 
+
+max_time = 0
+
+input_units = -1 
+
+print "----------------------- Batch NMR DAQ script ----------------------- \n" 
+# this reads inputs as integer, double, etc 
+input_units = input("Enter time units (0 = min, 1 = sec): ")
+max_time    = input("Enter maximum time to run:           ") 
+wait_time   = input("Enter time between runs:             ") 
+
+# get parameter configuration 
+print "Choose input parameter configuration (enter -1 if choice not available): "
+# os.system(list_configs)
+# os.listdir(config_dir)
+i = 0
+N = len(filenames) 
+for f in filenames:
+   print("%d: %s" %(i,f) )
+   i = i + 1  
+
+choice = input("Choice: ") 
+
+
+if(choice>=0 and choice<N): 
+   array = filenames[choice].split(".") 
+   config_name = array[0]
+else:
+   print "Configuration not present.  To create a configuration: "
+   print "1. Run the GUI, fill in the entry fields"
+   print "2. Click 'Apply Configuration' "
+   print "3. Click 'Quit' "
+   print "4. Re-run this script to enter batch mode. "
+   sys.exit(0) 
+
+# this reads input as a string 
+# config_name = raw_input("Enter configuaration name (no suffix): ")  
+print("Loading %s..." %(config_name) ) 
+
+myConfig.config = config_name
+
+myConfig.Print()  
+myConfig.CreateSymLinks() 
+
+unit = 0
+if input_units==0:
+   unit = minute
+elif input_units==1:
+   unit = sec 
+
+max_time   = max_time*unit  
+wait_time  = wait_time*unit
+t_start    = default_timer()
+t_duration = 0
+
+print( "max time:  %.3f s" %(max_time)  ) 
+print( "wait time: %.3f s" %(wait_time) )
+
+while t_duration <= max_time:
+   print( "Time passed: %.3f s" %(t_duration) )
+   # do stuff
+   # os.system(run_cmd) 
+   # wait specified time 
+   time.sleep(wait_time)
+   # get elapsed time to see if we keep taking data 
+   t_duration = default_timer() - t_start
+# end of loop 
+
+print "End of runs.  Exiting..."
+print "-------------------------------------------------------------------- \n" 
+
---
+++ confmgr.py
@@ -0,0 +1,95 @@
+# a class to set an NMR run configuration
+
+import os
+
+class ConfigManager:
+        #__________________________________________________________________
+        def __init__(self,home_path,config_name):
+                self.home     = home_path
+                self.config   = config_name
+                self.fpga     = "pulse-data" 
+                self.func_gen = "sg382" 
+                self.adc      = "struck_adc" 
+                self.util     = "utilities" 
+                self.com      = "comments" 
+                self.src_dir  = "/input/files/"
+                self.tgt_dir  = "/input/"
+        #__________________________________________________________________
+        def Print(self):
+                print( "[ConfigManager]: home               = %s" %(self.home)     )
+                print( "[ConfigManager]: config             = %s" %(self.config)   )
+                print( "[ConfigManager]: fpga file name     = %s" %(self.fpga)     ) 
+                print( "[ConfigManager]: func gen file name = %s" %(self.func_gen) ) 
+                print( "[ConfigManager]: adc file name      = %s" %(self.adc)      ) 
+                print( "[ConfigManager]: util file name     = %s" %(self.util)     ) 
+                print( "[ConfigManager]: comments file name = %s" %(self.com)      ) 
+                print( "[ConfigManager]: src directory      = %s" %(self.src_dir)  ) 
+                print( "[ConfigManager]: tgt directory      = %s" %(self.tgt_dir)  ) 
+        #__________________________________________________________________
+        def CreateSymLinks(self):  
+                # create symbolic links to the current configuration
+                cd_input  = "cd " + self.home + "/input/"
+                cd_home   = "cd " + self.home
+                symlink   = "ln -s "
+                # define the source files 
+                fpga_src  = self.home + self.src_dir + self.fpga     + "_" + self.config + ".dat" 
+                fg_src    = self.home + self.src_dir + self.func_gen + "_" + self.config + ".dat"  
+                adc_src   = self.home + self.src_dir + self.adc      + "_" + self.config + ".dat"  
+                util_src  = self.home + self.src_dir + self.util     + "_" + self.config + ".dat"   
+                com_src   = self.home + self.src_dir + self.com      + "_" + self.config + ".txt"  
+                # define targets  
+                fpga_tgt  = self.home + self.tgt_dir + self.fpga     + ".dat"
+                fg_tgt    = self.home + self.tgt_dir + self.func_gen + ".dat"
+                adc_tgt   = self.home + self.tgt_dir + self.adc      + ".dat"
+                util_tgt  = self.home + self.tgt_dir + self.util     + ".dat"
+                com_tgt   = self.home + self.tgt_dir + self.com      + ".txt"
+                # remove commands
+                rm_fpga   = "rm " + fpga_tgt
+                rm_fg     = "rm " + fg_tgt
+                rm_adc    = "rm " + adc_tgt
+                rm_util   = "rm " + util_tgt
+                rm_com    = "rm " + com_tgt
+                # check for existing files 
+                fpga_is_alive = os.path.islink(fpga_tgt)
+                fg_is_alive   = os.path.islink(fg_tgt)
+                adc_is_alive  = os.path.islink(adc_tgt)
+                util_is_alive = os.path.islink(util_tgt)
+                com_is_alive  = os.path.islink(com_tgt)
+                # delete current symbolic links if necessary 
+                if fpga_is_alive: os.remove( fpga_tgt )
+                if fg_is_alive:   os.remove( fg_tgt   )
+                if adc_is_alive:  os.remove( adc_tgt  )
+                if util_is_alive: os.remove( util_tgt )
+                if com_is_alive:  os.remove( com_tgt  )
+                # define the commands 
+                fpga_cmd  = symlink + fpga_src + " " + fpga_tgt   # symbolic link for FPGA 
+                fg_cmd    = symlink + fg_src   + " " + fg_tgt     # symbolic link for function generator  
+                adc_cmd   = symlink + adc_src  + " " + adc_tgt    # symbolic link for ADC 
+                util_cmd  = symlink + util_src + " " + util_tgt   # symbolic link for utilities 
+                com_cmd   = symlink + com_src  + " " + com_tgt    # symbolic link for comments
+                # print fpga_cmd 
+                # print fg_cmd 
+                # print adc_cmd 
+                # print util_cmd 
+                # print com_cmd 
+                # symbolically link files
+                os.system(cd_input)
+                os.system(fpga_cmd)
+                os.system(fg_cmd)
+                os.system(adc_cmd)
+                os.system(util_cmd)
+                os.system(com_cmd)
+                # check to see if commands succeeded 
+                fpga_is_alive = os.path.islink(fpga_tgt)
+                fg_is_alive   = os.path.islink(fg_tgt)
+                adc_is_alive  = os.path.islink(adc_tgt)
+                util_is_alive = os.path.islink(util_tgt)
+                com_is_alive  = os.path.islink(com_tgt)
+                if fpga_is_alive: print "[ConfigManager]: symbolic link from %s to %s created." % (fpga_src,fpga_tgt)
+                if fg_is_alive:   print "[ConfigManager]: symbolic link from %s to %s created." % (fg_src  ,fg_tgt  )
+                if adc_is_alive:  print "[ConfigManager]: symbolic link from %s to %s created." % (adc_src ,adc_tgt )
+                if util_is_alive: print "[ConfigManager]: symbolic link from %s to %s created." % (util_src,util_tgt)
+                if com_is_alive:  print "[ConfigManager]: symbolic link from %s to %s created." % (com_src ,com_tgt )
+                # cd back to main dir
+                os.system(cd_home)
+        #__________________________________________________________________
